bba37fef941603c1823089ea494d814457e0b1dc
Pranjal Gupta <109664273+72pranjal@users.noreply.github.com>
2024-12-16 15:36:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let allPrices = document.querySelectorAll('.price')
+    let sum = 0;
+    allPrices.forEach(item=> {
+        sum+= +item.innerText
+    })
+    console.log(sum)
+    let newRow = document.createElement('tr')
+    let tBody = document.querySelector('tbody')
+    tBody.append(newRow)
+    let newCell = document.createElement('td')
+    newRow.innerText = `Total Price ${sum}`
+    newRow.append(newCell)
   
 };
 
